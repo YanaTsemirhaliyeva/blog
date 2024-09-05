c6cc817e8844e32278b933a2fff725d7bf9e0d0e
Yana Temirgalieva <kolesiko.yana@gmail.com>
2024-09-05 20:50:06 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
